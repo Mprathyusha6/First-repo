f230af76412bbc28fb6f8282695625643194ae1c
Mprathyusha6 <74694018+Mprathyusha6@users.noreply.github.com>
2021-10-29 18:11:56 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,10 @@
+ 
+ 
+function demo( )
+{
+     
+    var x = document.getElementById("txtname").value;
+  
+    alert("Hello"+" "+x);
+}
+document.getElementById("btnName").addEventListener("click", demo);
